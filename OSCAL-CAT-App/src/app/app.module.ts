972d01eebe5eba178df8c667655a6593c4b92c6a
JustKuzya <dmitry.cousin@nist.gov>
2022-08-15 12:19:32 -0400

    Updated type and added baseline styles

--- OSCAL-CAT-App/src/app/app.module.ts
+++ OSCAL-CAT-App/src/app/app.module.ts
@@ -28,6 +28,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { IonicStorageModule } from '@ionic/storage-angular';
@@ -52,6 +54,7 @@ import { KnownOscalFilesService } from './providers/oscal-files/known-files.serv
     , ReactiveFormsModule
     , CommonModule
     , IonicModule
+    , HttpClientModule
     ,
   ],
 
