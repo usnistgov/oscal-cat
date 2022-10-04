0f19551313db90543eed862abee8724d03cb0788
JustKuzya <dmitry.cousin@nist.gov>
2022-10-04 19:06:32 -0400

    Null-wrapped meta pull-up in the page

--- OSCAL-CAT-App/src/app/pages/all-components/author-begin/author-begin.component.ts
+++ OSCAL-CAT-App/src/app/pages/all-components/author-begin/author-begin.component.ts
@@ -483,7 +483,6 @@ export class AuthorBeginComponent extends CatTheBaseComponent implements OnInit,
     if (!!this.savedWork) {
       // console.log(`Item Index ${theItemIndex} Event Target:${$event.target}`);
       const item = this.savedWork[theItemIndex]
-
       this.savedWork.splice(theItemIndex, 1);
       // **********************************************
       // Delete the HEAVY-WEIGHT session object by UUID
